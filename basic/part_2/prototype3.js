2ab27bfbea7705a982224c523ba6a13405454078
lucy <kkadangj@gmail.com>
2021-08-15 12:53:34 +0900

    [ADD] - instanceof , 프로토타입 프로퍼티 바인딩

--- basic/part_2/prototype3.js
+++ basic/part_2/prototype3.js
@@ -46,9 +46,55 @@ me.sayHello();
 console.log(Person.prototype === Object.getPrototypeOf(me));
 // 프로토타입으로 교체한 객체에는 constructor 프로퍼티가 없으므로 constructor 프로퍼티와 생성자 함수간의 연결이 파괴된다!
 
+// * instanceOf 연산지
+// 우변의 생성자 함수의 prototype 에 바인딩된 객체가 좌변의 객체의 프로토타입 체인 상에 존재하면 true로 평가되고, 그렇지 않은 경우에는 false 로 평가된다
+
+function School(name) {
+    this.name = name;
+}
+
+const school = new School("lucy School");
+
+// School.prototype 이 school 객체의 프로토타입 체인 상에 존재하므로
+console.log(school instanceof School);
+
+// Object.prototype 이 me 객체의 프로토타입 체인 상에 존재하므로 true 로 평가된다
+console.log(school instanceof Object);
+
+// * 어떻게 동작?
+
+function Cafe(name) {
+    this.name = name;
+    this.sayHello2 = function sayHello2() {
+        console.log(`${this.name} 입니다`)
+    }
+}
 
 
+const cafe = new Cafe("Twosome");
 
+
+cafe.sayHello2();
+// 프로토타입으로 교체할 객체
+const cafeParent = {};
+// 프로토타입의 교체
+
+Object.setPrototypeOf(cafe, cafeParent);
+
+console.log(cafe);
+// Person 생성자 함수와 parent 객체는 연결되어있지 않다
+console.log("Cafe", Cafe.prototype === cafeParent);
+console.log("cafeParent", cafeParent.constructor === Cafe);
+console.log(cafe instanceof Cafe); // false 연결파괴
+console.log(cafe instanceof Object); // true
+
+
+// Cafe.prototype 이 cafe 객체의 프로토타입 체인 상에 존재하지 않기 때문이다 따라서 프로토타입으로 교체한
+// cafe 객체를 Cafe 생성자 함수의 prototype 프로퍼티에 바인딩하면
+// cafe instanceof Cafe -> true
+Cafe.prototype = cafeParent;
+console.log(cafe instanceof Cafe); // 프로토타입 체인 상에 존재
+console.log(cafe instanceof Object);
 
 
 
