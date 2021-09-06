2e83fa3985d1ecd4018090b588b9717acf528953
lucy <johyunjin0905@gmail.com>
2021-09-06 21:50:27 +0900

    [ADD] - es6 메서드 2

--- basic/part_3/es6_add_feature/es6.js
+++ basic/part_3/es6_add_feature/es6.js
@@ -71,7 +71,38 @@ const obj3 = {
 // es6 사양에서 정의한 메서드(이하 es6메서드)는 인스턴스를 생성할 수 없는 non-consturctor 다
 //
 //new obj3.foo(); // TypeError: obj3.foo is not a constructor
-new obj3.bar();
+console.log(new obj3.bar()); // bar {}
 
 
+console.log(obj3.foo.hasOwnProperty('prototype')) // false
+console.log(obj3.bar.hasOwnProperty('prototype')) // true
 
+
+// * 표준 빌드인 객체가 제공하는 프로토타입 메서드와 정적 메서드는 모두 non-constuctor 이다
+console.log(String.prototype.toUpperCase.prototype);
+
+
+//es6 메서드는 자신을 바인딩한 객체를 가리키는 내부슬롯 [[HomeObject]]를 갖는다
+// super 참조는 내부 슬롯 [[HomeObject]]를 사용해서 슈퍼 클래스의 메서드를 참조 내부슬롯[[HomeObject]]를 갖는
+// ES6 메서드는 super 키워드를 사용할수 있다
+
+
+const base = {
+    name: "lucy",
+    sayHi() {
+        return `hi ${this.name}`
+    }
+}
+
+
+const derived = {
+    __proto__: base,
+    // sayHi 는 es6 메서드 es6 메서드는 [[HomeObject]] 갖고있다
+    // sayHi [[HomeObject]]는 derived.prototype 을 가리키도
+    // super는 sayHi의 [[HomeObject]]의 프로토타입인 base.prototype을 가리킨다
+    sayHi() {
+        return `${super.sayHi()}. how are you doing?`
+    }
+}
+
+console.log(derived.sayHi()); // hi! Lee. how are you doing?
