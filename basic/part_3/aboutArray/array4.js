20e4f76349482fef329a03ec792e0dd9c957c32d
lucy <johyunjin0905@gmail.com>
2021-09-25 14:21:24 +0900

    [ADD] - 배열 고차 함수 3

--- basic/part_3/aboutArray/array4.js
+++ basic/part_3/aboutArray/array4.js
@@ -2,10 +2,42 @@
 // 고차 함수는 함수를 인수로 전달받거나 함수를 반환하는 함수를 말한다 외부상태의 변경이나 가변 데이터를 피하고 불변성을 지향하는 함수형 프로그래밍에 기반 두고있다
 // 함수형 프로그래밍은 로직 내에 존재하는 조건문과 반복문제거하여 복잡성을 해결하고
 // 변수의 사용을 억제하여 상태 변경을 피하려는 프로그래밍 패러다임이다
+const originalArray = [ 1, 2, 3 ]
 
 // * Array.prototype.sort()
 // 배열의 요소를 정렬 , 원본배열을 직접 변경하여 정렬된 배열을 반환 기본적으로 오름차순
 console.log("Array.prototype.sort() ====================================")
+
+/**
+ * [ sort ]
+ */
+
+originalArray.sort((prev, next) => {
+    return next - prev
+})
+console.log(originalArray);
+
+function sort(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - i; j++) {
+            if (!array[j + 1]) {
+                break;
+            }
+
+            const result = callback(array[j], array[j + 1]);
+            if (result > 0) {
+                // i, j 스위치
+                let temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
+sort(originalArray, (prev, next) => prev - next);
+console.log(originalArray);
+
 const fruit = ["Banna", "Apple", "kiwi"];
 fruit.sort();
 console.log("fruit 원본배열을 직접 변경", fruit);
@@ -59,6 +91,25 @@ console.log(todos);
 // * Array.prototype.forEach()
 // 배경
 console.log("Array.prototype.forEach() ====================================")
+
+/**
+ * [ forEach ]
+ * for문을 추상화 시켜놓은 함수
+ */
+originalArray.forEach((value) => {
+    console.log(value)
+})
+
+function forEach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        callback(array[i], i, array);
+    }
+}
+
+forEach(originalArray, (value) => {
+    console.log(value)
+})
+
 const numbers = [1, 2, 3];
 const pows = [];
 
@@ -143,11 +194,34 @@ const sparseArr = [1, ,3];
 sparseArr.forEach(v => console.log(v));
 
 
+
 // * Array.prototype.map()
 // map 메서드는 자신을 호출한 배열의 모든 요소를 순회하면서 인수로 전달받은 콜백함수를 반복 호출한다
 // 그리고 콜백함수의 반환값들로 구성된 새로운 배열을 반환한다
 // 원본 배열 변경되지 않는다
 console.log("Array.prototype.map() ====================================")
+/**
+ * [ map ]
+ * 특정 배열을 다른 배열로 변환 or mapping 해주는 함수
+ */
+const mappedArray = originalArray.map((value) => `${value}`);
+console.log(mappedArray, originalArray === mappedArray)
+
+function map(callback) {
+    const array = this;
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const newValue = callback(array[i], i, array);
+        // (array[i]) => `${array[i]}`
+        newArray.push(newValue);
+    }
+    return newArray;
+}
+
+const myMap = map.bind(originalArray);
+const myMappedArray = myMap((value) => `${value + 1}`)
+console.log(myMappedArray)
+
 const mapNumbers = [1, 2, 3];
 const roots = mapNumbers.map(item => Math.sqrt(item))
 // const roots = mapNumbers.map(Math.sqrt) 와 같다
@@ -177,6 +251,8 @@ class Prefixer {
 }
 const priefixer = new Prefixer('-webkit');
 console.log(priefixer.add[('transition', 'user-select')])
+
+
 
 
 // * Array.prototype.filter()
@@ -232,6 +308,26 @@ console.log(user)
 
 // * Array.prototype.reduce()
 console.log("Array.prototype.reduce() ====================================")
+/**
+ * [ reduce ]
+ * for 문을 돌면서 값을 누적시키는 함수
+ */
+console.log(
+    originalArray.reduce((accumulator, value) => accumulator + value, 0)
+)
+
+function reduce(array, callback, initialValue) {
+    let accumulator = initialValue;
+    for (let i = 0; i < array.length; i++) {
+        accumulator = callback(accumulator, array[i], i, array)
+    }
+    return accumulator;
+}
+
+console.log(
+    reduce(originalArray, (accumulator, value) => accumulator + value, 0)
+)
+
 const sum = [1, 2, 3, 4].reduce((accumulator, currentValue, index, array) => accumulator + currentValue, 0)
 console.log(sum)
 
@@ -278,8 +374,118 @@ console.log(reduceRemoveValue3)
 const filterRemoveValue = values3.filter((v,i,arr) => arr.indexOf(v) === i)
 console.log(filterRemoveValue);
 
+// 중복되지 않는 유일한 값들의 집합인 Set 을 사용할수도 있다 중복 요소를 제거할때 추천
+// 중복 허용하지 않는 set객체의 특성을 활용하여 배열에서 중복된 요소를 제거할수 있다
+const setRemoveResult = [...new Set(values3)]
+console.log(setRemoveResult);
+
+let reduceSum = [1, 2, 3, 4].reduce((acc, cur) => acc + cur, 0);
+console.log(reduceSum);
+// 하지만 reduce 는 초기값 전달하는것이 안전하다
+// reduceSum = [].reduce((acc, cur) => acc + cur);
+// array4.js:289 Uncaught TypeError: Reduce of empty array with no initial value
+reduceSum = [].reduce((acc, cur) => acc + cur, 0);
+console.log(reduceSum);
+
+// 객체 특정프로퍼티 값을 합산하는 경우
+ const product = [
+     {id: 1, price: 100},
+     {id: 2, price: 200},
+     {id: 3, price: 300}
+ ]
+// 첫번째 순회시 acc는 { id: 1, price: 100 } cur은 { id: 2, price: 200 } 이고
+// 두번째 순회시 acc 는 300 cur { id: 3, price 300 이다}
+// 두번째 순회시 acc함수에 객체가 아닌 숫자값이 전달된다 이때  acc.price -> undefined
+let priceSum = product.reduce((acc, cur) => acc.price + cur.price);
+console.log(priceSum)
+// 반드시 초기값 설정
+
+/*
+* 1번째 순회 : acc => 0,    cur => { id: 1, price: 100 }
+* 2번째 순회 : acc => 100,  cur => { id: 2, price: 200 }
+* 3번째 순회 : acc => 300,  cur => { id: 3, price: 300 }
+* */
+let priceSum2 = product.reduce((acc, cur) => acc + cur.price, 0);
+console.log(priceSum2)
+
+
+
+
 // * Array.prototype.some()
+console.log("Array.prototype.some() ====================================")
+// 자신의 호출한 배열의 요소를 순회하면서 인수로 전달된 콜백 함수를 호출한다
+// 배열의 요소 중 10보다 큰 요소가 1개 이상 존재하는지 확인
+console.log([5, 10, 15].some(item => item > 10))
+// 배열의 요소 중 0보다 작은 요소가 1개 이상 존재하는지 확인
+console.log([5, 10, 15].some(item => item < 0))
+// 배열의 요소 중 바나나가 한개 이상 존재 하는 확인
+console.log(["banna", "apple"].some(itme => itme === "banna"))
+// 빈배열은 항상 false
+console.log([].some(data => data))
+
+
+
 // * Array.prototype.every()
+console.log("Array.prototype.every() ====================================")
+// 자신을 호출한 배열의 요소를 순회하면서 인수로 전달된 콜백함수로 호출한다
+// 배열의 요소 중 10보다 큰 요소가 1개 이상 존재하는지 확인
+console.log([5, 10, 15].every(item => item > 10))
+// 배열의 요소 중 0보다 작은 요소가 1개 이상 존재하는지 확인
+console.log([5, 10, 15].every(item => item > 0))
+// 배열의 요소 중 바나나가 한개 이상 존재 하는 확인
+console.log(["banna", "banna"].every(itme => itme === "banna"))
+// 빈배열은 항상 true
+console.log([].every(data => data > 3))
+
+
+
 // * Array.prototype.find()
+console.log("Array.prototype.find() ====================================")
+// 자신을 호출한 배열의 요소를 순회하면서 인수로 전달된 콜백함수를 호출하여 반환값이 true인 첫번째 요소를 반환한다
+
+const users2 = [
+    { id: 1, name: "lucy" },
+    { id: 2, name: "hyun" },
+    { id: 3, name: "jin" }
+];
+console.log(users2.find(user => user.id === 2));
+// 하지만 find메서드는 콜백함수의 반환값이 true 첫번쨰 요소를 반환하므로 find의 결과값은 배열이 아닌 해당 요소값이다
+console.log([ 1, 2, 2, 3 ].filter(item => item === 2)) // 배열반환
+console.log([ 1, 2, 2, 3 ].find(item => item === 2)) // 첫번째 요소 반환
+
+// 두번째 인수로 this 전달 가능
+
+
+
 // * Array.prototype.findIndex()
+console.log("Array.prototype.findIndex() ====================================")
+
+// es6 도입된 findIndex 메서드는 자신을 호출한 배열의 요소를 순회하면서 인수로 전달된 콜백 함수를 호출하여 반환값이 true인 첫번째 요소의 인덱스를 반환한다
+// 콜백함수의 반환값이 true인 요소가 존재하지 않는다면 -1 반환한다
+console.log(user.findIndex(user => user.id === 2));
+console.log(user.findIndex(user => user.name === "jo"));
+
+// 추상화
+function predicate(key, value) {
+    return item => item[key] === value;
+}
+
+console.log(users2.findIndex(predicate('id', 2)));
+console.log(users2.findIndex(predicate('name', 'jin')));
+
+
 // * Array.prototype.flatMap()
+console.log("Array.prototype.flatMap() ====================================")
+// es10
+const arr = ['hello', 'world'];
+
+// map flat을 순차적으로 실행
+console.log(arr.map(x => x.split('')).flat())
+console.log(arr.flatMap(x => x.split('')));
+
+// 평탄화 깊이를 지정하려면 map 과 flat 지정하기 (flatMap 1단계만 평탄화 한다)
+console.log(arr.flatMap((str, index) => [index, [str, str.length]]));
+console.log(arr.map((str, index) => [index, [str, str.length]]).flat(2));
+
+
+
