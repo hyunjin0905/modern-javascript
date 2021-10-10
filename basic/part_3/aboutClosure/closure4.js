8ae41eb95d5dce85c8237c019ede7f2444fe8f8c
lucy <johyunjin0905@gmail.com>
2021-10-10 09:14:30 +0900

    [MOD] - 4

--- basic/part_3/aboutClosure/closure4.js
+++ basic/part_3/aboutClosure/closure4.js
@@ -1,3 +1,21 @@
 // 캡슐화 정보 은닉
-// 객체의 상태를 나태는 프로퍼티와 프로퍼티와 참조하고 조작할수 있는 동작인 메서드를 하나로 묶는것
+// 캡슐화 객체의 상태를 나타내는 프로퍼티와 프로퍼티와 참조하고 조작할수 있는 동작인 메서드를 하나로 묶는것
 // 객체의 특정 프로퍼티나 메서드를 감출 목적으로 사용하기도 하는데 이를 정보 은닉이라 한다
+
+function Person(name, age) {
+    this.name = name; // public
+    let _age = age; // private
+
+    // 인스턴스 메서드
+    Person.prototype.sayHi = function () {
+        console.log(`${this.name}  ${_age}`);
+    }
+
+}
+const me = new Person("lee", 20);
+me.sayHi();
+console.log(me.name)
+console.log(me._age)
+
+
+
