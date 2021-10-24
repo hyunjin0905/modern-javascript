99941014feba2eefaf296aa29ee7ab048eaa727c
lucy <johyunjin0905@gmail.com>
2021-10-24 20:39:08 +0900

    [ADD]

--- basic/part_5/aboutPromise/promise3.js
+++ basic/part_5/aboutPromise/promise3.js
@@ -28,3 +28,9 @@ promiseGet('https://jsonplaceholder.typicode.com/posts/1')
 // rejected           비동기 처리가 수행된 상태(실패)           reject 함수 호출
 // 생성된 직후의 프로미스는 기본적으로 pending 상태
 
+// fullfilled된 프로미스
+const fullfilled = new Promise(resolve => resolve(1));
+// rejected된 프로미스
+const rejected = new Promise((_, rejected) => rejected(new Error("error occurred")))
+
+// 프로미스는 비동기처리 상태와 처리 결과를 관리하는 객체다 
