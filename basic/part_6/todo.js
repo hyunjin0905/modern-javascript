7992096e1bdbc924de639dc448dfbcb8b8642550
lucy <johyunjin0905@gmail.com>
2021-11-14 13:27:58 +0900

    [ADD] - 구조분해 [ADD] - todo manager

--- basic/part_6/todo.js
+++ basic/part_6/todo.js
@@ -36,3 +36,4 @@ class TodoManager {
         ,0)
     }
 }
+console.log(new TodoManager)
--- basic/part_6/todoApp.js
+++ basic/part_6/todoApp.js
@@ -1,10 +1,64 @@
+
+
 class TodoApp {
     constructor(todos) {
+        console.log(todos)
         this.todoManager = new TodoManager(todos);
+        console.log(new TodoManager(todos))
         this.todoContainerElem = document.querySelector(".todo-container");
+        this.titleEl = document.querySelector(".title h2");
+        this.plusBtnEl = document.querySelector(".add_todo button");
+        console.log(this.plusBtnEl)
+        this.renderTodos();
+        this.bindEvents();
+    }
 
+    renderTodos () {
+        this.todoContainerElem.innerHTML = "";
+        this.todoManager.getList().forEach((todo, index) => {
+            const todoEl = this.createTodoEl(todo, index);
+            this.todoContainerElem.appendChild(todoEl);
+        })
+        this.renderTitle()
+    }
 
+    createTodoEl(todo, id) {
+        const todoEl = document.createElement("div");
+        todoEl.id = "todo- " + id;
+        todoEl.className = "todo";
+        todoEl.innerHTML = `<input type="checkbox" ${todo.done? "checked": "" }> <label>${todo.contents}</label>`
+        console.log(todoEl)
+        return todoEl;
+    }
 
+    renderTitle() {
+        const now = new Date();
+        const month = now.getMonth();
+        const date = now.getDate();
+
+        if (this.titleEl) {
+            this.titleEl.innerHtml =
+                `${month}월 ${date}일 <span class="left-count"> (${this.todoManager.leftTodoCount }) </span>`
+        }
+    }
+
+    bindEvents() {
+        console.log(this.plusbtnEl)
+        this.plusBtnEl.addEventListener("click", (event) => {
+            var textEl = document.querySelector('.add_todo input[type="text"]');
+            this.todoManager.addTodo(textEl.value);
+            textEl.value = "";
+            this.renderTodos();
+        })
+        this.todoContainerEl.addEventListener("click", (event) => {
+            if (event.target.nodeName === "INPUT" && event.target.parentElement.className === "todo") {
+                const clickEl = event.target.parentElement,
+                      index = clickedEl.id.replace("todo-", "")
+                this.todoManager.getList()[index].toggle()
+                this.renderTitle()
+
+            }
+        })
     }
 
 }
